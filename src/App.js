8b3207b26f875fd42d8d7ad64776efec7ac973ad
Jonathan Ho <johnnyho82@gmail.com>
2019-08-14 09:43:31 -0400

    Fix bug in large number output and add increase = functionality

--- src/App.js
+++ src/App.js
@@ -21,26 +21,34 @@ function App() {
   const [firstNum, setFirstNum] = useState(null);
   const [operation, setOperation] = useState(null);
   const [secondNum, setSecondNum] = useState(null);
-  const [functional, setFunctional] = useState(true);
+  const [newOperation, setNewOperation] = useState(true);
   const [numInputOn, setNumInputOn] = useState(true);
 
+  // Re-initialize state
+  const reInitState = () => {
+    setDisplay('');
+    setFirstNum(null);
+    setSecondNum(null);
+    setOperation(null);
+    setNewOperation(true);
+    setNumInputOn(true);
+  };
+
+  // Set functionality of special buttons
   const specialFunctions = input => {
+    // Re-initializes state and restore functionality
     if (input === 'C') {
-      setDisplay('');
-      setFirstNum(null);
-      setSecondNum(null);
-      setOperation(null);
-      setFunctional(true);
-      setNumInputOn(true);
-    } else if (functional && input === '+/-') {
+      reInitState();
+    } else if (newOperation && input === '+/-') {
       setDisplay(display * -1);
-    } else if (functional && input === '%') {
+    } else if (newOperation && input === '%') {
       setDisplay(display / 100);
     }
   };
 
+  // Takes input as a string then converts to number with maximum 14 digits
   const numDisplay = input => {
-    if (functional && numInputOn) {
+    if (newOperation && numInputOn) {
       const inputArr = [];
       inputArr.push(display.toString());
       inputArr.push(input.toString());
@@ -49,22 +57,28 @@ function App() {
     }
   };
 
+  // Functionality for operation buttons; //! need to update logic for operation chaining
   const operator = input => {
-    if (functional) {
+    if (newOperation) {
       if (input !== '=') {
         setNumInputOn(true);
         setFirstNum(display);
         setOperation(input);
         setDisplay('');
       } else if (input === '=') {
-        setFunctional(false);
+        setNewOperation(false);
         setSecondNum(display);
+      }
+    } else if (!newOperation) {
+      if (input === '=') {
+        setFirstNum(display);
       }
     }
   };
 
+  // Limits display output of large numbers and irrational numbers in decimal form
   const lengthCheck = input => {
-    if (input > 99999999999999) {
+    if (input > 9999999999999) {
       input = input.toExponential(8);
     } else if (input.toString().length >= 14) {
       input = input.toFixed(13 - input.toString().split('.')[0].length);
@@ -72,27 +86,24 @@ function App() {
     return input;
   };
 
+  // Calculate operations and update display
   useEffect(() => {
     if (secondNum) {
       if (operation === '+') {
         let value = firstNum + secondNum;
-        value = lengthCheck(value);
-        setDisplay(value);
+        setDisplay(lengthCheck(value));
       } else if (operation === '-') {
         let value = firstNum - secondNum;
-        value = lengthCheck(value);
-        setDisplay(value);
+        setDisplay(lengthCheck(value));
       } else if (operation === 'x') {
         let value = firstNum * secondNum;
-        value = lengthCheck(value);
-        setDisplay(value);
+        setDisplay(lengthCheck(value));
       } else if (operation === '/') {
         let value = firstNum / secondNum;
-        value = lengthCheck(value);
-        setDisplay(value);
+        setDisplay(lengthCheck(value));
       }
     }
-  }, [secondNum, operation, firstNum, display]);
+  }, [secondNum, operation, firstNum]);
 
   return (
     <div className="container">
@@ -110,10 +121,11 @@ function App() {
           </div>
         </div>
       </div>
-      {/* <h1>Display: {display}</h1>
+      <h1>New Operation: {newOperation ? 'Yes' : 'No'}</h1>
+      <h1>Display: {display}</h1>
       <h1>FirstNum: {firstNum}</h1>
       <h1>Operator: {operation}</h1>
-      <h1>SecondNum: {secondNum}</h1> */}
+      <h1>SecondNum: {secondNum}</h1>
     </div>
   );
 }
